c32231e7b76dbb8cb111d0de72b0047e2c6df90c
Mike Gallagher <mike.gallagh@gmail.com>
2020-03-25 20:49:44 -0500

    add login emoji to login

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -55,7 +55,7 @@ class NavBar extends React.Component {
               <a
                 href="/auth/google"
                 className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
-                Login
+                Login 📚
               </a>
             ) : (
               <a
