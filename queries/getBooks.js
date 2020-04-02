7d40e27da40facc6cb90cfdb6545e56f8ace3e49
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-02 01:17:15 -0500

    ugh, don't use select count

--- queries/getBooks.js
+++ queries/getBooks.js
@@ -4,7 +4,7 @@ module.exports = id => {
   return new Promise((resolve, reject) => {
     const oldQuery = `SELECT * FROM user_books WHERE user_books.user_id = ${id};`;
 
-    const query = `SELECT COUNT(*) FROM user_books 
+    const query = `SELECT * FROM user_books 
     WHERE user_id 
     IN (SELECT DISTINCT user_id FROM households_users WHERE invite_accepted = true AND household_id 
     IN (SELECT household_id FROM households_users WHERE user_id = ${id})) OR user_id = ${id};`;
