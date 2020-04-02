6318c94d72ae0c91f8c6bb8621e67e808c8da165
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-02 00:30:44 -0500

    get all books initially across all households

--- queries/getBooks.js
+++ queries/getBooks.js
@@ -2,8 +2,14 @@ const db = require('../config/db/mysql').pool;
 
 module.exports = id => {
   return new Promise((resolve, reject) => {
+    const oldQuery = `SELECT * FROM user_books WHERE user_books.user_id = ${id};`;
+    const query = `SELECT * FROM user_books 
+    WHERE user_id 
+    IN (SELECT DISTINCT user_id FROM households_users WHERE household_id 
+    IN (SELECT household_id FROM households_users WHERE user_id = ${id}));`;
+
     db.query(
-      `SELECT * FROM user_books WHERE user_books.user_id = ${id};`,
+      query,
       (err, books, fields) => {
         if (err) {
           reject(err);
