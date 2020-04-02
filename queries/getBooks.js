e8f9116d8b3c3c617be53814be8d2163ca9328c3
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-02 01:12:58 -0500

    move invite_accepted into first subquery

--- queries/getBooks.js
+++ queries/getBooks.js
@@ -4,10 +4,10 @@ module.exports = id => {
   return new Promise((resolve, reject) => {
     const oldQuery = `SELECT * FROM user_books WHERE user_books.user_id = ${id};`;
 
-    const query = `SELECT * FROM user_books 
+    const query = `SELECT COUNT(*) FROM user_books 
     WHERE user_id 
-    IN (SELECT DISTINCT user_id FROM households_users WHERE household_id 
-    IN (SELECT household_id FROM households_users WHERE invite_accepted = true AND is_owner = false AND user_id = ${id})) OR user_id = ${id};`;
+    IN (SELECT DISTINCT user_id FROM households_users WHERE invite_accepted = true AND household_id 
+    IN (SELECT household_id FROM households_users WHERE user_id = ${id})) OR user_id = ${id};`;
 
     db.query(
       query,
