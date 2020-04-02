596b6e7cf724300a5f3312a8950cd9f72c64206f
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-02 00:58:19 -0500

    remove extraneous limit on query

--- queries/getBooks.js
+++ queries/getBooks.js
@@ -3,10 +3,11 @@ const db = require('../config/db/mysql').pool;
 module.exports = id => {
   return new Promise((resolve, reject) => {
     const oldQuery = `SELECT * FROM user_books WHERE user_books.user_id = ${id};`;
+    
     const query = `SELECT * FROM user_books 
     WHERE user_id 
     IN (SELECT DISTINCT user_id FROM households_users WHERE household_id 
-    IN (SELECT household_id FROM households_users WHERE invite_accepted = true AND user_id = ${id})) AND invite_accepted = true OR user_id = ${id};`;
+    IN (SELECT household_id FROM households_users WHERE invite_accepted = true AND user_id = ${id})) OR user_id = ${id};`;
 
     db.query(
       query,
