5f631dc9af1e7942ed3db5a6d4bc9e8caaa697cf
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-21 21:27:36 -0500

    working query fo removing household member

--- queries/removeHouseholdMember.js
+++ queries/removeHouseholdMember.js
@@ -2,7 +2,10 @@ const db = require('../config/db/mysql').pool;
 
 module.exports = (householdId, userId) => {
   return new Promise((resolve, reject) => {
-    const query = `DELETE households_users, households_books FROM households_books LEFT JOIN households_users ON households_users.household_id = households_books.household_id WHERE households_books.user_id = ${userId} AND households_books.household_id = ${householdId} AND households_users.user_id = ${userId}`;
+    const query = `DELETE households_users, households_books 
+    FROM households_users 
+    LEFT JOIN households_books ON households_users.household_id = households_books.household_id 
+    WHERE households_users.user_id = ${userId} AND households_books.user_id = ${userId}`;
     console.log(query)
     db.query(query, (err, results, fields) => {
       if (err) throw Error(err);
