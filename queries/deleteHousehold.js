529217593354051d7d5510d94406a0c93704fcc6
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-21 20:07:22 -0500

    remove a user, their household books

--- queries/deleteHousehold.js
+++ queries/deleteHousehold.js
@@ -2,8 +2,8 @@ const db = require('../config/db/mysql').pool;
 
 module.exports = (householdId) => {
   return new Promise((resolve, reject) => {
-    const query = `DELETE households, households_users, households_books FROM households INNER JOIN households_books INNER JOIN households_users ON households.id = households_users.household_id WHERE households.id = ${householdId} AND households_users.household_id = ${householdId}`;
-
+    const query = `DELETE households, households_users, households_books FROM households LEFT JOIN households_books ON households_books.household_id = households.id LEFT JOIN households_users ON households_users.household_id = households.id WHERE households.id = ${householdId}`;
+    console.log(query);
     db.query(query, (err, results, fields) => {
       if (err) throw Error(err);
       resolve(results);
--- queries/removeHouseholdMember.js
+++ queries/removeHouseholdMember.js
@@ -2,7 +2,7 @@ const db = require('../config/db/mysql').pool;
 
 module.exports = (householdId, userId) => {
   return new Promise((resolve, reject) => {
-    const query = `DELETE households_users, households_books FROM households_users INNER JOIN households_books ON households_books.household_id = households_users.household_id WHERE households_users.user_id = ${userId} AND households_users.household_id = ${householdId}`;
+    const query = `DELETE households_users, households_books FROM households_books LEFT JOIN households_users ON households_users.household_id = households_books.household_id WHERE households_books.user_id = ${userId} AND households_books.household_id = ${householdId} AND households_users.user_id = ${userId}`;
     console.log(query)
     db.query(query, (err, results, fields) => {
       if (err) throw Error(err);
