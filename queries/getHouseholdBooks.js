144c4f434417fb1b875d5264fd748b637e96e49e
Mike Gallagher <mike.gallagh@gmail.com>
2020-04-28 15:22:25 -0500

    add todo file and change qeury to get household books

--- queries/getHouseholdBooks.js
+++ queries/getHouseholdBooks.js
@@ -1,9 +1,10 @@
 const db = require('../config/db/mysql').pool;
 
+
 module.exports = (userId) => {
 
   return new Promise((resolve, reject) => {
-    const query = `SELECT global_books.*, households_books.notes as household_notes, households_books.user_id, households_books.household_id FROM households_users JOIN households_books ON households_books.household_id = households_users.household_id JOIN global_books ON global_books.id = households_books.global_book_id WHERE households_users.user_id = ${userId} AND households_books.user_id != ${userId}`;
+    const query = `SELECT global_books.*, households_books.notes as household_notes, households_books.user_id, households_books.household_id, households_books.id AS households_book_id FROM households_users JOIN households_books ON households_books.household_id = households_users.household_id JOIN global_books ON global_books.id = households_books.global_book_id WHERE households_users.user_id = ${userId} AND households_books.user_id != ${userId}`;
 
     console.log(query)
 
