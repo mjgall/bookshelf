ffd12c1ce08525aa03acf8366a273a8702d6914c
Mike Gallagher <mike.gallagh@gmail.com>
2020-05-12 23:48:26 -0500

    fix bug re:
    deleting member would delete entire household

--- queries/removeHouseholdMember.js
+++ queries/removeHouseholdMember.js
@@ -2,10 +2,12 @@ const db = require('../config/db/mysql').pool;
 
 module.exports = (householdId, userId) => {
   return new Promise((resolve, reject) => {
-    const query = `DELETE households_users, households_books 
+    const query = `DELETE households_users, households_books
     FROM households_users 
     LEFT JOIN households_books ON households_users.user_id = households_books.user_id 
-    WHERE households_users.household_id = ${householdId} AND households_books.household_id = ${householdId}`;
+    WHERE households_users.household_id = ${householdId} 
+    AND households_books.household_id = ${householdId} 
+    AND households_users.user_id = ${userId};`;
 
     db.query(query, (err, results, fields) => {
       if (err) throw Error(err);
@@ -13,9 +15,3 @@ module.exports = (householdId, userId) => {
     });
   });
 };
-
-
-// `DELETE households_users, households_books 
-//     FROM households_users 
-//     LEFT JOIN households_books ON households_users.household_id = households_books.household_id 
-//     WHERE households_users.user_id = ${userId} AND households_books.user_id = ${userId}`
