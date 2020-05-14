04684b84ce5004b37ec0a3de5ba089878802d59d
Mike Gallagher <mike.gallagh@gmail.com>
2020-05-13 23:01:50 -0500

    update meta description

--- queries/removeHouseholdMember.js
+++ queries/removeHouseholdMember.js
@@ -1,7 +1,9 @@
 const db = require('../config/db/mysql').pool;
 
 module.exports = (householdId, userId) => {
+
   return new Promise((resolve, reject) => {
+    
     const query = `DELETE households_users, households_books
     FROM households_users 
     LEFT JOIN households_books ON households_users.user_id = households_books.user_id 
@@ -13,5 +15,6 @@ module.exports = (householdId, userId) => {
       if (err) throw Error(err);
       resolve(results);
     });
+    
   });
 };
