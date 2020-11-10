28ad6734773b82c254fd6a9691a23ff37c1f48e9
Mike Gallagher <mike.gallagh@gmail.com>
2020-11-09 18:01:11 -0600

    get activities from users reading global books

--- queries/getActivities.js
+++ queries/getActivities.js
@@ -9,8 +9,16 @@ module.exports = (userId) => {
         JOIN activities ON users.id = activities.user_id
         JOIN global_books ON global_books.id = activities.object_id
         JOIN user_books ON global_books.id = user_books.global_id AND user_books.user_id = users.id AND user_books.private = FALSE
-        WHERE ( friendships.user_id_1 = ${userId}
-            OR friendships.user_id_2 = ${userId} ) ORDER BY activities.timestamp DESC;`
+        WHERE ( friendships.user_id_1 = ${userId} OR friendships.user_id_2 = ${userId} )
+UNION
+SELECT DISTINCT activities.*, global_books.title, users.id AS friend_id, users.email AS friend_email, users.full AS friend_full, users.picture AS friend_picture FROM friendships 
+        JOIN users 
+            ON ((friendships.user_id_1 = users.id OR friendships.user_id_2 = users.id) AND (friendships.accepted = TRUE AND friendships.declined != TRUE))
+        JOIN activities ON users.id = activities.user_id
+        JOIN global_books ON global_books.id = activities.object_id
+        JOIN users_globalbooks ON users_globalbooks.user_id = activities.user_id 
+        WHERE ( friendships.user_id_1 = ${userId} OR friendships.user_id_2 = ${userId} )
+ORDER BY timestamp DESC`
         db.query(query, (err, results, fields) => {
             if (err) {
                 reject(err);
