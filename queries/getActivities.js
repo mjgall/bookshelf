a42550a11fc8d007d3994c9a7dc09adb88b0c17b
Mike Gallagher <mike.gallagh@gmail.com>
2020-11-10 23:29:35 -0600

    show that you read friends global books

--- queries/getActivities.js
+++ queries/getActivities.js
@@ -12,13 +12,13 @@ module.exports = (userId) => {
         WHERE ( friendships.user_id_1 = ${userId} OR friendships.user_id_2 = ${userId} )
 UNION
 SELECT DISTINCT activities.*, global_books.title, global_books.cover, users.id AS friend_id, users.email AS friend_email, users.full AS friend_full, users.picture AS friend_picture FROM friendships 
-        JOIN users 
+        JOIN users
             ON ((friendships.user_id_1 = users.id OR friendships.user_id_2 = users.id) AND (friendships.accepted = TRUE AND friendships.declined != TRUE))
         JOIN activities ON users.id = activities.user_id
         JOIN global_books ON global_books.id = activities.object_id
         JOIN users_globalbooks ON users_globalbooks.user_id = activities.user_id AND users_globalbooks.global_book_id = activities.object_id
-        JOIN user_books ON users_globalbooks.global_book_id = user_books.global_id
-        WHERE ( friendships.user_id_1 = ${userId} OR friendships.user_id_2 = ${userId} ) AND users.id != ${userId}
+        JOIN user_books ON users_globalbooks.global_book_id = user_books.global_id AND users.id != user_books.user_id
+        WHERE ( friendships.user_id_1 = ${userId} OR friendships.user_id_2 = ${userId} )
 ORDER BY timestamp DESC`
         db.query(query, (err, results, fields) => {
             if (err) {
