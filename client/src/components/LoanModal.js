add0730f5d61538413802d57ac8b1bd280a53e63
Mike Gallagher <mike.gallagh@gmail.com>
2021-06-13 19:33:52 -0500

    include message when you don't have connections on loan modal

--- client/src/components/LoanModal.js
+++ client/src/components/LoanModal.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { Context } from "../globalContext";
+import { Link } from "react-router-dom";
 const LoanModal = (props) => {
 	const global = useContext(Context);
 	const [connections, setConnections] = useState([]);
@@ -27,33 +28,60 @@ const LoanModal = (props) => {
 		let updatedAllBooks = [...global.allBooks];
 		updatedAllBooks[index].on_loan = 1;
 		updatedAllBooks[index].borrower_id = friendId;
-		updatedAllBooks[index].full = friendName
+		updatedAllBooks[index].full = friendName;
 		global.setGlobal({ allBooks: updatedAllBooks });
 
-		global.setGlobal({modalOpen: false})
+		global.setGlobal({ modalOpen: false });
 	};
 
 	return (
 		<>
 			<div className="flex flex-wrap py-6 px-8">
 				<div>
-					<div className="text-lg font-weight-bold">Loan to:</div>
-					{connections.map((connection, index) => {
-						return (
-							<div
-								onClick={() => loanTo(connection.user_id, connection.full)}
-								key={index}
-								className="flex items-center my-2 cursor-pointer"
-							>
-								<img
-									alt={connection.full}
-									src={connection.picture}
-									className="rounded-full h-12 w-12 mr-4"
-								></img>
-								<div>{connection.full}</div>
+					{connections.length < 1 ? (
+						<div className="text-center text my-1 text-gray-500 italic font-weight-light">
+							<div>
+								You don't have any connections, create a
+								household or connect with friends from
 							</div>
-						);
-					})}
+							<Link
+								onClick={() =>
+									global.setGlobal({ modalOpen: false })
+								}
+								className="border-gray-600 border-dotted border-b"
+								to="/profile"
+							>
+								your profile.
+							</Link>
+						</div>
+					) : (
+						<>
+							<div className="text-lg font-weight-bold">
+								Loan to:
+							</div>
+							{connections.map((connection, index) => {
+								return (
+									<div
+										onClick={() =>
+											loanTo(
+												connection.user_id,
+												connection.full
+											)
+										}
+										key={index}
+										className="flex items-center my-2 cursor-pointer"
+									>
+										<img
+											alt={connection.full}
+											src={connection.picture}
+											className="rounded-full h-12 w-12 mr-4"
+										></img>
+										<div>{connection.full}</div>
+									</div>
+								);
+							})}
+						</>
+					)}
 				</div>
 			</div>
 		</>
