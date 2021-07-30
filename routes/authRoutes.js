d59bfeed861da044fa6f24b3650c21e2c5b62504
Mike Gallagher <mike.gallagh@gmail.com>
2021-07-30 00:59:18 -0500

    prevent existing emails from registering

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,74 +1,76 @@
-const passport = require('passport');
-const addUser = require('../queries/addUser')
+const passport = require("passport");
+const addUser = require("../queries/addUser");
+const getUserByEmail = require("../queries/getUserByEmail");
 
 module.exports = (app) => {
-  app.get('/auth/google/redirect/:referrer/', (req, res, next) => {
+  app.get("/auth/google/redirect/:referrer/", (req, res, next) => {
     req.session.redirect = req.params.referrer;
 
-    passport.authenticate('google', {
-      scope: ['profile', 'email'],
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
     })(req, res, next);
   });
 
-  app.get('/auth/google/redirect/book/owned/:id', (req, res, next) => {
-    req.session.redirect = 'book/owned' + '/' + req.params.id;
+  app.get("/auth/google/redirect/book/owned/:id", (req, res, next) => {
+    req.session.redirect = "book/owned" + "/" + req.params.id;
 
-    passport.authenticate('google', {
-      scope: ['profile', 'email'],
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
     })(req, res, next);
   });
 
-  app.get('/auth/google/redirect/book/household/:id', (req, res, next) => {
-    req.session.redirect = 'book/household' + '/' + req.params.id;
- 
-    passport.authenticate('google', {
-      scope: ['profile', 'email'],
+  app.get("/auth/google/redirect/book/household/:id", (req, res, next) => {
+    req.session.redirect = "book/household" + "/" + req.params.id;
+
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
     })(req, res, next);
   });
 
-  app.get('/auth/google/redirect/:referrer/:id', (req, res, next) => {
-    req.session.redirect = req.params.referrer + '/' + req.params.id;
+  app.get("/auth/google/redirect/:referrer/:id", (req, res, next) => {
+    req.session.redirect = req.params.referrer + "/" + req.params.id;
 
-    passport.authenticate('google', {
-      scope: ['profile', 'email'],
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
     })(req, res, next);
   });
 
-  app.get('/auth/google', (req, res, next) => {
+  app.get("/auth/google", (req, res, next) => {
     req.session.redirect = null;
-    passport.authenticate('google', {
-      scope: ['profile', 'email'],
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
     })(req, res, next);
   });
 
   app.get(
-    '/auth/google/callback',
-    passport.authenticate('google'),
+    "/auth/google/callback",
+    passport.authenticate("google"),
     (req, res) => {
       if (req.session.redirect) {
         const redirect = req.session.redirect;
         req.session.redirect = null;
         res.redirect(`/${redirect}`);
       } else {
-        res.redirect('/');
+        res.redirect("/");
       }
     }
   );
-  
-  app.post('/auth/transparent', (req, res, next) => {
 
+  app.post("/auth/transparent", (req, res, next) => {
     if (!req.user.admin) {
-      res.sendStatus(401)
+      res.sendStatus(401);
     } else {
-      passport.authenticate('local', (err, user, info) => {
+      passport.authenticate("local", (err, user, info) => {
         if (err) {
-          return res.status(400).send({ error: 'Something went wrong.' });
+          return res
+            .status(400)
+            .send({ error: "Something went wrong." });
         }
         if (!user) {
           return res.status(200).send(info);
         }
-  
-        req.logIn(user, function(err) {
+
+        req.logIn(user, function (err) {
           if (err) {
             return next(err);
           }
@@ -78,31 +80,31 @@ module.exports = (app) => {
     }
   });
 
-  app.get('/api/logout', (req, res) => {
+  app.get("/api/logout", (req, res) => {
     req.session.redirect = null;
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
   });
 
-  app.get('/api/transparent_logout', (req, res) => {
+  app.get("/api/transparent_logout", (req, res) => {
     req.session.redirect = null;
     req.logout();
   });
 
-  app.get('/api/current_user', (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 
-  app.post('/auth/login', (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
+  app.post("/auth/login", (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
       if (err) {
-        return res.status(400).send({ error: 'Something went wrong.' });
+        return res.status(400).send({ error: "Something went wrong." });
       }
       if (!user) {
         return res.status(200).send(info);
       }
 
-      req.logIn(user, function(err) {
+      req.logIn(user, function (err) {
         if (err) {
           return next(err);
         }
@@ -111,18 +113,29 @@ module.exports = (app) => {
     })(req, res, next);
   });
 
-  app.post('/auth/register', async (req, res) => {
-    const { password, firstName, lastName, email } = req.body;
+  app.post("/auth/register", async (req, res) => {
+    const { firstName, lastName } = req.body;
     try {
-      await addUser({...req.body, first: firstName, last: lastName, googleId: null, picture: null})
-      res.status(200).send({ message: 'success' });
+      const users = await getUserByEmail(req.body.email);
+
+      if (users) {
+        res.status(400).send({ message: "Email already exists" });
+      } else {
+        await addUser({
+          ...req.body,
+          first: firstName,
+          last: lastName,
+          googleId: null,
+          picture: null,
+        });
+        res.status(200).send({ message: "success" });
+      }
     } catch (error) {
-      console.log(error)
-      res.sendStatus(500)
+      console.log(error);
+      res.sendStatus(500);
     }
 
     // console.log(req.body)
     // res.send(req.body)
   });
-
 };
