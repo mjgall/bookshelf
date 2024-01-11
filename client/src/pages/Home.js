fdc3ce1066d34f7fbf3a8650d585db85c890273b
Mike Gallagher <mike@gllghr.io>
2024-01-11 08:14:55 -0600

    remove clubs path access when not enabled

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,72 +13,77 @@ import Cancellation from "./Cancellation";
 import Clubs from "./Clubs";
 import Club from "./Club";
 
+import { useFeatureFlagEnabled } from "posthog-js/react";
+
 const Home = (props) => {
-  const global = useContext(Context);
-  const match = useRouteMatch();
+	const global = useContext(Context);
+	const match = useRouteMatch();
+	const clubsEnabled = useFeatureFlagEnabled("clubs");
 
-  useEffect(() => {
-    if (props.location.state?.redirect) {
-      // addRedirect(props.location.state.from)
-      global.setGlobal({
-        redirect: props.location.state.from,
-      });
-    }
+	useEffect(() => {
+		if (props.location.state?.redirect) {
+			// addRedirect(props.location.state.from)
+			global.setGlobal({
+				redirect: props.location.state.from,
+			});
+		}
 
-    if (global.currentUser?.id) {
-      localStorage.setItem("existingUser", true);
-    }
-  }, [props, global]);
+		if (global.currentUser?.id) {
+			localStorage.setItem("existingUser", true);
+		}
+	}, [props, global]);
 
-  return (
-    <>
-      {global.currentUser ? (
-        <div className="w-full max-w-screen-xl mx-auto my-4">
-          <div
-            className="md:grid"
-            style={{
-              gridTemplateColumns: "10% 88%",
-              gridColumnGap: "2%",
-            }}
-          >
-            <div className="md:bg-transparent">
-              <Subnav currentPage={match.url}></Subnav>
-            </div>
-            <div className="border-gray-400 border rounded py-3 px-3 shadow-lg md:mx-0 mx-2 bg-white">
-              <Switch>
-                <Route path="/library">
-                  <Library></Library>
-                </Route>
-                <Route path="/feed">
-                  <Feed></Feed>
-                </Route>
-                <Route path="/account">
-                  <Account></Account>
-                </Route>
-                <Route path="/loans">
-                  <Loans></Loans>
-                </Route>
-                <Route path="/clubs/:id">
-                  <Club></Club>
-                </Route>
-                <Route path="/clubs">
-                  <Clubs></Clubs>
-                </Route>
-                <Route path="/cancel">
-                  <Cancellation></Cancellation>
-                </Route>
-                <Route path="/*">
-                  <Library></Library>
-                </Route>
-              </Switch>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <MarketingHome redirect={global.redirect}></MarketingHome>
-      )}
-    </>
-  );
+	return (
+		<>
+			{global.currentUser ? (
+				<div className="w-full max-w-screen-xl mx-auto my-4">
+					<div
+						className="md:grid"
+						style={{
+							gridTemplateColumns: "10% 88%",
+							gridColumnGap: "2%",
+						}}
+					>
+						<div className="md:bg-transparent">
+							<Subnav currentPage={match.url}></Subnav>
+						</div>
+						<div className="border-gray-400 border rounded py-3 px-3 shadow-lg md:mx-0 mx-2 bg-white">
+							<Switch>
+								<Route path="/library">
+									<Library></Library>
+								</Route>
+								<Route path="/feed">
+									<Feed></Feed>
+								</Route>
+								<Route path="/account">
+									<Account></Account>
+								</Route>
+								<Route path="/loans">
+									<Loans></Loans>
+								</Route>
+								<Route path="/clubs/:id">
+									<Club></Club>
+								</Route>
+								{clubsEnabled ? (
+									<Route path="/clubs">
+										<Clubs></Clubs>
+									</Route>
+								) : null}
+								<Route path="/cancel">
+									<Cancellation></Cancellation>
+								</Route>
+								<Route path="/*">
+									<Library></Library>
+								</Route>
+							</Switch>
+						</div>
+					</div>
+				</div>
+			) : (
+				<MarketingHome redirect={global.redirect}></MarketingHome>
+			)}
+		</>
+	);
 };
 
 export default withRouter(Home);
